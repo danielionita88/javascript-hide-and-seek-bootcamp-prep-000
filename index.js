a2a0fe3cd13fcc998b0f8c217c7932719bd93835
fortuitous-map-5512 <danielionita88@yahoo.com>
2019-08-30 15:32:52 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
+function getFirstSelector(selector) {
+  return document.querySelector(`${selector}`);
+}
 
+function nestedTarget() {
+  return document.querySelector('#nested .target');
+}
+
+function increaseRankBy(n) {
+  var rankedList= document.querySelectorAll(".ranked-list li");
+  
+  for( let i = 0; i < rankedList.length; i++){
+    rankedList[i].innerHTML = parseInt(rankedList[i].innerHTML) + n;
+  }
+  
+}
+
+
+function deepestChild() {
+  var deepest = document.getElementById('grand-node').querySelectorAll('div')
+  return deepest[deepest.length-1]
+}
